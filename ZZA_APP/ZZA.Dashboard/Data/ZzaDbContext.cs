64df409e503a83eb4691476480481be709de5479
Michael Marango <mike.marango@gmail.com>
2019-11-11 21:00:46 +0300

    Add db context and configuration

---
+++ ZZA_APP/ZZA.Dashboard/Data/ZzaDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using ZZA.Models;
+
+namespace ZZA.Dashboard.Data
+{
+    public class ZzaDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<OrderItemOption> OrderItemOptions { get; set; }
+        public DbSet<ProductOption> ProductOptions { get; set; }
+        public DbSet<ProductSize> ProductSizes { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .Property(c => c.Id)
+                .ValueGeneratedNever();
+
+            var cascadeFKs = modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership 
+                && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+            foreach (var fk in cascadeFKs)
+                fk.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            _ = optionsBuilder
+                .UseSqlServer(ConfigurationManager.ConnectionStrings["zza.db"].ConnectionString);
+        }
+    }
+}
